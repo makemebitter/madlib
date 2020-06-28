224d88b6730da6e008a13de56c17064d16a94389
Yuhao <yhzhang1994@gmail.com>
2020-06-28 16:07:42 -0500

    update

---
+++ tool/cluster_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+root=${1:-"/local/madlib"}
+hosts=${2:-"/local/gphost_list"}
+# Make compile dir
+cd "$root"
+mkdir -p build
+# Compile
+cd build
+cmake ..
+make -j
+# Copy to every host
+readarray -t hostsarr < $hosts
+
+for host in "${hostsarr[@]}"; do
+    echo "Syncing $host ..."
+    rsync -ar --update --delete "$root/build" "gpadmin@$host:$root/build"
+done
+
+# Install
+$root/build/src/bin/madpack -p greenplum -c gpadmin@master:5432/cerebro reinstall
+
